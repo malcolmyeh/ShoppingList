05a20b2be1ff9348aeda13f6f3a55ff1f018310f
Malcolm Yeh <malcolmyeh@gmail.com>
2020-07-18 17:04:53 -0400

    Updates

---
+++ precache-manifest.41fde9a53eeee88eaafeecacd5c11dbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5fc50b1fdc16990818c04cf83ceea7b",
+    "url": "/ShoppingList/index.html"
+  },
+  {
+    "revision": "29c24d8c9af231270075",
+    "url": "/ShoppingList/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7de114a048693bd4fd70",
+    "url": "/ShoppingList/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "29c24d8c9af231270075",
+    "url": "/ShoppingList/static/js/2.5a32246e.chunk.js"
+  },
+  {
+    "revision": "dd2bafddf84d0013ad828f5f5d6dbbaf",
+    "url": "/ShoppingList/static/js/2.5a32246e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7de114a048693bd4fd70",
+    "url": "/ShoppingList/static/js/main.69873b4d.chunk.js"
+  },
+  {
+    "revision": "b62e4202082163fd9a11",
+    "url": "/ShoppingList/static/js/runtime-main.afc1ded6.js"
+  }
+]);
